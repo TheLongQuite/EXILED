2806dd3f78cc22e97a3a9b543ef0a045e30e56a2
IRacle <zornik2006@yandex.ru>
2023-03-08 17:23:28 +0300

    revert some cringe

--- Exiled.CustomRoles/API/Features/CustomRole.cs
+++ Exiled.CustomRoles/API/Features/CustomRole.cs
@@ -98,11 +98,6 @@ namespace Exiled.CustomRoles.API.Features
         public virtual SpawnProperties SpawnProperties { get; set; } = new();
 
         /// <summary>
-        /// Gets or sets a value indicating whether players keep their current position when gaining this role.
-        /// </summary>
-        public virtual bool KeepPositionOnSpawn { get; set; }
-
-        /// <summary>
         /// Gets or sets a value indicating whether players keep their current inventory when gaining this role.
         /// </summary>
         public virtual bool KeepInventoryOnSpawn { get; set; }
@@ -427,28 +422,29 @@ namespace Exiled.CustomRoles.API.Features
                         player.Position = pos + (Vector3.up * 1.5f);
                     }
 
-            if (!KeepInventoryOnSpawn)
-            {
-                Log.Debug($"{Name}: Clearing {player.Nickname}'s inventory.");
-                player.ClearInventory();
-            }
+                    if (!KeepInventoryOnSpawn)
+                    {
+                        Log.Debug($"{Name}: Clearing {player.Nickname}'s inventory.");
+                        player.ClearInventory();
+                    }
 
-            foreach (string itemName in Inventory)
-            {
-                Log.Debug($"{Name}: Adding {itemName} to inventory.");
-                TryAddItem(player, itemName);
-            }
+                    foreach (string itemName in Inventory)
+                    {
+                        Log.Debug($"{Name}: Adding {itemName} to inventory.");
+                        TryAddItem(player, itemName);
+                    }
 
-            foreach (AmmoType ammo in Ammo.Keys)
-            {
-                Log.Debug($"{Name}: Adding {Ammo[ammo]} {ammo} to inventory.");
-                player.SetAmmo(ammo, Ammo[ammo]);
-            }
+                    foreach (AmmoType ammo in Ammo.Keys)
+                    {
+                        Log.Debug($"{Name}: Adding {Ammo[ammo]} {ammo} to inventory.");
+                        player.SetAmmo(ammo, Ammo[ammo]);
+                    }
 
-            Log.Debug($"{Name}: Setting health values.");
-            player.Health = MaxHealth;
-            player.MaxHealth = MaxHealth;
-            player.Scale = Scale;
+                    Log.Debug($"{Name}: Setting health values.");
+                    player.Health = MaxHealth;
+                    player.MaxHealth = MaxHealth;
+                    player.Scale = Scale;
+                });
 
             Log.Debug($"{Name}: Setting player info");
             player.CustomInfo = CustomInfo;
@@ -461,6 +457,7 @@ namespace Exiled.CustomRoles.API.Features
 
             ShowMessage(player);
             RoleAdded(player);
+            TrackedPlayers.Add(player);
             player.UniqueRole = Name;
             player.TryAddCustomRoleFriendlyFire(Name, CustomRoleFFMultiplier);
         }

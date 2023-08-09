e53868dea87595e5c1fbf8c288f62dd0aec9aba7
IRacle <zornik2006@yandex.ru>
2023-08-09 20:00:31 +0300

    e

--- Exiled.CustomRoles/Commands/List/InGame.cs
+++ Exiled.CustomRoles/Commands/List/InGame.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="Tracked.cs" company="Exiled Team">
+// <copyright file="InGame.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>

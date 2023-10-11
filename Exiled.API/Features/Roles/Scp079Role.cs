f0e11bc7ceeafa22c6df24dc68744ded51fd89f3
IRacle <zornik2006@yandex.ru>
2023-10-11 14:48:24 +0300

    test

--- Exiled.API/Features/Roles/Scp079Role.cs
+++ Exiled.API/Features/Roles/Scp079Role.cs
@@ -268,7 +268,7 @@ namespace Exiled.API.Features.Roles
         public int Level
         {
             get => TierManager.AccessTierLevel;
-            set => Experience = value <= 1 ? 0 : TierManager.AbsoluteThresholds[Mathf.Clamp(value - 2, 0, TierManager.AbsoluteThresholds.Length - 1)];
+            set => Experience = value <= 1 ? 0 : TierManager.AbsoluteThresholds[Mathf.Clamp(value - 1, 0, TierManager.AbsoluteThresholds.Length - 1)];
         }
 
         /// <summary>

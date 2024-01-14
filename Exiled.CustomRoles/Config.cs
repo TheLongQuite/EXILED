40ac2f6dafaa6ceb832cdbe0ea481ef41121baad
KrisPrs <dubenskiy04@gmail.com>
2024-01-14 16:55:13 +0300

    Правочка

--- Exiled.CustomRoles/Config.cs
+++ Exiled.CustomRoles/Config.cs
@@ -21,47 +21,58 @@ namespace Exiled.CustomRoles
     /// </summary>
     public class Config : IConfig
     {
+        /// <inheritdoc />
+        [Description("Whether or not the plugin is enabled.")]
+        public bool IsEnabled { get; set; } = true;
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether debug messages should be printed to the console.
+        /// </summary>
+        /// <returns><see cref="bool" />.</returns>
+        [Description("Whether or not debug messages should be shown.")]
+        public bool Debug { get; set; } = false;
+
         /// <summary>
         ///     Gets the hint that is shown when someone gets a <see cref="CustomRole" />.
         /// </summary>
         [Description("The hint that is shown when someone gets a custom role.")]
-        public Hint GotRoleHint { get; } = new("You have spawned as a {0}\n{1}", 6);
+        public Hint GotRoleHint { get; set; } = new("You have spawned as a {0}\n{1}", 6);
 
         /// <summary>
         ///     Gets the hint that is shown when someone used a <see cref="CustomAbility" />.
         /// </summary>
         [Description("The hint that is shown when someone used a custom ability.")]
-        public Hint UsedAbilityHint { get; } = new("Ability {0} has been activated.\n{1}", 5);
+        public Hint UsedAbilityHint { get; set; } = new("Ability {0} has been activated.\n{1}", 5);
 
         /// <summary>
         ///     Gets the hint that is shown when someone used a <see cref="CustomAbility" />.
         /// </summary>
         [Description("The hint that is shown when someone's custom ability is ready.")]
-        public Hint AbilityReadyHint { get; } = new("Ability {0} is ready.\n{1}", 5);
+        public Hint AbilityReadyHint { get; set; } = new("Ability {0} is ready.\n{1}", 5);
 
         /// <summary>
         ///     Gets the hint that is shown when someone used a cooldowned <see cref="CustomAbility" />.
         /// </summary>
         [Description("The hint that is shown when someone tries to use cooldowned ability. Also used in console respond. {0} - remaining cooldown, {1} - ability name")]
-        public Hint AbilityOnCooldownHint { get; } = new("Способность на перезарядке!\nПодождите ещё {0} секунд перед использованием.", 5);
+        public Hint AbilityOnCooldownHint { get; set; } = new("Способность на перезарядке!\nПодождите ещё {0} секунд перед использованием.", 5);
 
         /// <summary>
         ///     Gets the hint that is shown when someone tries to use <see cref="CustomAbility" /> without required energy.
         /// </summary>
         [Description("The hint that is shown when someone tries to use ability without required energy. Also used in console respond. {0} - current energy, {1} - required energy")]
-        public Hint InsufficientEnergyHint { get; } = new("Недостаточно энергии!\nУ вас {0}/{1}", 5);
+        public Hint InsufficientEnergyHint { get; set; } = new("Недостаточно энергии!\nУ вас {0}/{1}", 5);
 
         /// <summary>
         ///     Gets the hint that is shown when someone tries to use <see cref="CustomAbility" /> without required level.
         /// </summary>
         [Description("The hint that is shown when someone tries to use ability without required level. Also used in console respond. {0} - current level, {1} - required level")]
-        public Hint InsufficientLevelHint { get; } = new("Недостаточный уровень!\nУ вас {0}/{1}", 5);
+        public Hint InsufficientLevelHint { get; set; } = new("Недостаточный уровень!\nУ вас {0}/{1}", 5);
 
         /// <summary>
         ///     Gets the hint that is shown when someone tries to use <see cref="CustomAbility" /> with too high level.
         /// </summary>
         [Description("The hint that is shown when someone tries to use ability with too high level. Also used in console respond. {0} - current level, {1} - required level")]
-        public Hint RedundantLevelHint { get; } = new("Избыточный уровень!\nУ вас {0}/{1}", 5);
+        public Hint RedundantLevelHint { get; set; } = new("Избыточный уровень!\nУ вас {0}/{1}", 5);
 
         /// <summary>
         ///     Gets or sets response of <see cref="RoleInfo" />.
@@ -122,16 +133,5 @@ namespace Exiled.CustomRoles
         /// </summary>
         [Description("Задержка до синхронизации имён кастомных ролей и наблюдателей.")]
         public float CustomRolesSpectatorDisplayDelay { get; set; } = 2;
-
-        /// <inheritdoc />
-        [Description("Whether or not the plugin is enabled.")]
-        public bool IsEnabled { get; set; } = true;
-
-        /// <summary>
-        ///     Gets or sets a value indicating whether debug messages should be printed to the console.
-        /// </summary>
-        /// <returns><see cref="bool" />.</returns>
-        [Description("Whether or not debug messages should be shown.")]
-        public bool Debug { get; set; } = false;
     }
 }

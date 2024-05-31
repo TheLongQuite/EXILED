e673b60e262bca2116753bd89fbe2093cb7bb217
KrisPrs <dubenskiy04@gmail.com>
2024-05-31 20:35:18 +0300

    Размер неверен

--- Exiled.API/Features/Player.cs
+++ Exiled.API/Features/Player.cs
@@ -378,7 +378,7 @@ namespace Exiled.API.Features
                 }
 
                 if (value != null && value.Contains('<'))
-                    value = $"<size=100%>{value}</size>";
+                    value = $"<size=17>{value}</size>";
 
                 InfoArea = string.IsNullOrEmpty(value) ? InfoArea & ~PlayerInfoArea.CustomInfo : InfoArea |= PlayerInfoArea.CustomInfo;
                 ReferenceHub.nicknameSync.Network_customPlayerInfoString = value;

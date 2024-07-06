1aa37e9a7f183d255e0685460a3918975bc8df2c
IRacle <zornik2006@yandex.ru>
2024-07-06 22:33:36 +0300

    tyler

--- Exiled.Events/Patches/Fixes/FixPickupSyncInfoEqualityCheck.cs
+++ Exiled.Events/Patches/Fixes/FixPickupSyncInfoEqualityCheck.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="FixPickupPreviousOwner.cs" company="Exiled Team">
+// <copyright file="FixPickupSyncInfoEqualityCheck.cs" company="Exiled Team">
 // Copyright (c) Exiled Team. All rights reserved.
 // Licensed under the CC BY-SA 3.0 license.
 // </copyright>

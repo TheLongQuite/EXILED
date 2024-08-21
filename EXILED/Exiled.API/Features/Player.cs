fe9195b369c2585001c6a3ac5ab6839e397f4cc1
IRacle <zornik2006@yandex.ru>
2024-08-21 12:58:12 +0300

    Revert "Возврат к той страшной штуке"
    This reverts commit 3cd633febd5ec7f6b7b1f66f300258cc8ff6ad5d.

--- EXILED/Exiled.API/Features/Player.cs
+++ EXILED/Exiled.API/Features/Player.cs
@@ -1258,13 +1258,8 @@ namespace Exiled.API.Features
             if (Dictionary.TryGetValue(gameObject, out Player player))
                 return player;
 
-            if (UnverifiedPlayers.TryGetValue(gameObject, out player))
-                return player;
-
-            if (ReferenceHub.TryGetHub(gameObject, out ReferenceHub hub) && ReferenceHub.LocalHub != hub)
-                return new(hub);
-
-            return null;
+            UnverifiedPlayers.TryGetValue(gameObject, out player);
+            return player;
         }
 
         /// <summary>
